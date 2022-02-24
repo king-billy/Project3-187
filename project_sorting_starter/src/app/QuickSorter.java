3742921813fcd53309179e94b661abaf52e0fe4f
king-billy <anshulsingh1312@gmail.com>
2022-02-24 13:10:43 -0500

    quickSort algo

--- project_sorting_starter/src/app/QuickSorter.java
+++ project_sorting_starter/src/app/QuickSorter.java
@@ -11,7 +11,28 @@ public class QuickSorter<T> extends AbstractSorter<T> {
 
 	@Override
 	public SwapList<T> sort() {
-		// TODO sort
-		return null;
+		quickSort(0, list.size() - 1);
+		
+		return list;
+	}
+
+	public int partition(int low, int high){
+		int i = low - 1;
+		for(int j = low; j <= high - 1;j++){
+			if(list.compare(j, high, comparator) < 0){
+				i++;
+				list.swap(i, j);
+			}
+		}
+		list.swap(i+1, high);
+		return (i+1);
+	}
+
+	public void quickSort(int low, int high){
+		if(low < high){
+			int pi = partition(low, high);
+			quickSort(low, pi-1);
+			quickSort(pi+1, high);
+		}
 	}
 }
